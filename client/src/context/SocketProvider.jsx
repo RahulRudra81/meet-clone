29b575f78f7bcad1fc4a1bd1c5d51cdc9f674dfd
rahulrudra81 <rahulrudra81@gmail.com>
2024-02-03 14:45:15 +0530

    added vercel.json

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket=()=>{
 }
 
 export const SocketProvider =(props)=>{
-    const socket=useMemo(()=>io("localhost:3000"),[])
+    const socket=useMemo(()=>io("https://meet-clone-lcg7.onrender.com"),[])
     return(
         <SocketContext.Provider value={socket}>
             {props.children}
